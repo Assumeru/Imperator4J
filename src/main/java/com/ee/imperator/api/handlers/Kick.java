b2c6217008ed8f5077172a447469948c42da384a
Assumeru <malusluminis@hotmail.com>
2016-05-18 19:18:53 +0200

    kick

---
+++ src/main/java/com/ee/imperator/api/handlers/Kick.java
@@ -0,0 +1,26 @@
+package com.ee.imperator.api.handlers;
+
+import com.ee.imperator.Imperator;
+import com.ee.imperator.exception.InvalidRequestException;
+import com.ee.imperator.exception.RequestException;
+import com.ee.imperator.game.Game;
+import com.ee.imperator.user.Member;
+import com.ee.imperator.user.Player;
+
+@Request(mode = "game", type = "kick")
+public class Kick {
+	public void handle(Member member, @Param("gid") int gid, @Param("uid") int uid) throws RequestException {
+		Game game = Imperator.getData().getGame(gid);
+		if(game == null) {
+			throw new InvalidRequestException("Game does not exist", "game", "kick");
+		} else if(!game.getOwner().equals(member)) {
+			throw new InvalidRequestException("Only the game owner can kick players.", "game", "kick");
+		} else if(member.getId() == uid) {
+			throw new InvalidRequestException("You cannot kick yourself.", "game", "kick");
+		}
+		Player player = game.getPlayerById(uid);
+		if(player != null) {
+			Imperator.getData().removePlayerFromGame(player, game);
+		}
+	}
+}
