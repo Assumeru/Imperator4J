522aa7bfa076fa18935eb97d8b016b8f62765723
Assumeru <malusluminis@hotmail.com>
2016-08-22 19:24:25 +0200

    reuse securerandom instances

--- src/main/java/com/ee/imperator/crypt/bcrypt/BCryptHasher.java
+++ src/main/java/com/ee/imperator/crypt/bcrypt/BCryptHasher.java
@@ -1,5 +1,6 @@
 package com.ee.imperator.crypt.bcrypt;
 
+import org.ee.web.ApplicationContext;
 import org.mindrot.jbcrypt.BCrypt;
 
 import com.ee.imperator.ImperatorApplicationContext;
@@ -7,14 +8,16 @@ import com.ee.imperator.crypt.PasswordHasher;
 
 public class BCryptHasher implements PasswordHasher {
 	private final int rounds;
+	private final ApplicationContext context;
 
 	public BCryptHasher(ImperatorApplicationContext context) {
 		rounds = context.getConfig().getInt(getClass(), "rounds", 10);
+		this.context = context;
 	}
 
 	@Override
 	public String hash(String password) {
-		return BCrypt.hashpw(password, BCrypt.gensalt(rounds));
+		return BCrypt.hashpw(password, BCrypt.gensalt(rounds, context.getSecureRandom()));
 	}
 
 	@Override
